4c9b15907431bb1cfb1d69f7926f60d46ca1f981
kitsonE04 <kitsonE04@gmail.com>
2022-11-22 23:00:31 +0000

    s

--- js/search.js
+++ js/search.js
@@ -13,11 +13,32 @@ const searchShow = (event) => {
         .then((data) => {
             data.forEach(function (value) {
                 console.log(value);
-                const tvshowElement = `<div class="col-md-3">
-                <div class="card bg-transparent mb-4"><div class="card-body">
-                <a console.log(${value.show.id}) href="results.html">
-                <img alt="back-image" class="rounded" src="${value.show.image.medium}">
-                </div></div></div>`;
+                const tvshowElement = `<div class="col-md-12">
+                <div class="card bg-dark mb-4">
+                    <div class="card-body"> 
+                        <div class="container">
+                            <h1 class="card-title" style="color:white;">${value.show.name}</h1>
+                            <div class="container"></div>
+                            <div class="row">
+                            <div class="col-3">
+                                <img alt="back-image" class="rounded mt-4" src="${value.show.image.medium}">
+                            </div>
+                            <div class="col-5">
+                                <p class="card-text">${value.show.summary}</p>
+                            </div>
+                            <div class="col-4" style="color:white">
+                                <div class="text-bg-danger rounded p-3">
+                                <h3 class="card-text" bg-black>
+                                <div class="row mb-2">Rating: ${value.show.rating.average}</div>
+                                <div class="row mb-2">Genre: ${value.show.genre}</div>
+                                <div class="row mb-2">Status: ${value.show.status}</div>
+                                <div class="row mb-2">Runtime: ${value.show.averageRuntime}</div>
+                                </h3></div>
+                            </div></div>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
                 resultList.insertAdjacentHTML('beforeend', tvshowElement); 
             });
         });
