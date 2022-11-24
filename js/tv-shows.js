e2e8b8ac93cb260ee592d159e0aa1543f531c06a
kitsonE04 <kitsonE04@gmail.com>
2022-11-24 12:08:00 +0000

    h

--- js/tv-shows.js
+++ js/tv-shows.js
@@ -14,13 +14,13 @@ fetch(url)
             const showElement = `
             <div class="col-md-12">
                 <div class="card bg-dark mb-4">
-                    <div class="card-body" onclick="seasonUrl = 'https://api.tvmaze.com/shows/${value.id}/seasons'; console.log(seasonUrl); fetch(seasonUrl)"> 
+                    <div class="card-body" href="results.html" onclick="seasonUrl = 'https://api.tvmaze.com/shows/${value.id}/seasons'; console.log(seasonUrl);"> 
                         <div class="container">
                             <h1 class="card-title" style="color:white;">${value.name}</h1>
                             <div class="container"></div>
                             <div class="row">
                             <div class="col-2">
-                                <img alt="back-image" class="rounded mt-4" src="${value.image.medium}">
+                            <a target="_blank" href="results.html"><img alt="back-image" class="rounded mt-4" src="${value.image.medium}"></a>
                             </div>
                             <div class="col-6">
                                 <p class="card-text">${value.summary}</p>
@@ -28,10 +28,10 @@ fetch(url)
                             <div class="col-4" style="color:white">
                                 <div class="text-bg-danger rounded p-3">
                                 <h3 class="card-text" bg-black>
-                                <div class="row mb-2">Rating: ${value.rating.average}/10</div>
-                                <div class="row mb-2">Genre: ${value.genre}</div>
-                                <div class="row mb-2">Status: ${value.status}</div>
-                                <div class="row mb-2">Runtime: ${value.averageRuntime} minutes</div>
+                                <div class="row mb-2"><u>Rating:</u> ${value.rating.average}/10</div>
+                                <div class="row mb-2"><u>Genre:</u> ${value.genres}</div>
+                                <div class="row mb-2"><u>Status:</u> ${value.status}</div>
+                                <div class="row mb-2"><u>Runtime:</u> ${value.averageRuntime} minutes</div>
                                 </h3></div>
                             </div></div>
                         </div>
@@ -41,7 +41,4 @@ fetch(url)
             resultList.insertAdjacentHTML('beforeend', showElement);
         });
     });
-
-
-
 
