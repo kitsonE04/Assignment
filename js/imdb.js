14c119e6a92d2c4ee62e9844a6affc6da27ffda7
kitsonE04 <kitsonE04@gmail.com>
2022-11-25 18:06:57 +0000

    j

--- js/imdb.js
+++ js/imdb.js
@@ -5,6 +5,20 @@ const showUrl = 'https://api.tvmaze.com/shows';
 const actorsUrl = 'https://api.tvmaze.com/people?page=1&api-key=';
 
 
+function getSeasons(){
+    const queryStr = seasonUrl;
+    const usp = new URLSearchParams(queryStr);
+
+
+    const showID = usp.get('id');
+    const showName = usp.get('name');
+
+    console.log(`id = ${showID} name = ${showName}`)
+
+    console.log(usp.toString())
+}
+
+
 fetch(showUrl)
     .then((response) => response.json())
     .then((data) => {
@@ -14,25 +28,25 @@ fetch(showUrl)
             const showElement = `
             <div class="col-md-12">
                 <div class="card bg-dark mb-4">
-                    <div class="card-body"> 
+                    <div class="card-body" onclick=" seasonUrl= 'id=${value.id}&name=${value.name}'; getSeasons() ;"> 
                         <div class="container">
                             <h1 class="card-title" style="color:white;">${value.name}</h1>
                             <div class="container"></div>
                             <div class="row">
                             <div class="col-2">
-                            <a target="_blank" href="results.html"><img alt="back-image" class="rounded mt-4" src="${value.image.medium}"></a>
+                            <a target="_blank"><img alt="back-image" class="rounded mt-4" src="${value.image.medium}"></a>
                             </div>
                             <div class="col-6">
                                 <p class="card-text">${value.summary}</p>
                             </div>
                             <div class="col-4" style="color:white">
                                 <div class="text-bg-danger rounded p-3">
-                                <h3 class="card-text" bg-black>
-                                <div class="row mb-2"><u>Rating:</u> ${value.rating.average}/10</div>
-                                <div class="row mb-2"><u>Genre:</u> ${value.genres}</div>
-                                <div class="row mb-2"><u>Status:</u> ${value.status}</div>
-                                <div class="row mb-2"><u>Runtime:</u> ${value.averageRuntime} minutes</div>
-                                </h3></div>
+                                <h4 class="card-text" bg-black>
+                                <div class="row mb-2">Rating: ${value.rating.average}/10</div>
+                                <div class="row mb-2">Genre: ${value.genres}</div>
+                                <div class="row mb-2">Status: ${value.status}</div>
+                                <div class="row mb-2">Runtime: ${value.averageRuntime} minutes</div>
+                                </h4></div>
                             </div></div>
                         </div>
                     </div>
@@ -49,7 +63,7 @@ fetch(showUrl)
             data.forEach(function (value) {
                 console.log(value);
                 const actorElement = `
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <div class="card bg-dark mb-4"><div class="card-body">
                     <h5 class="card-title" style="color:white;">${value.name}</h5>
                     <a href="results.html">
@@ -79,7 +93,7 @@ const searchShow = (event) => {
                             <div class="container"></div>
                             <div class="row">
                             <div class="col-2">
-                            <a target="_blank" href="results.html"><img alt="back-image" class="rounded mt-4" src="${value.show.image.medium}"></a>
+                            <a target="_blank"><img alt="back-image" class="rounded mt-4" src="${value.show.image.medium}"></a>
                             </div>
                             <div class="col-6">
                                 <p class="card-text">${value.show.summary}</p>
